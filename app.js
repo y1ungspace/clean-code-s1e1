a6b1154aa0973da1db726bc57c2dad7b77629ccb
y1ungspace <y1ung.space@gmail.com>
2022-05-29 17:46:44 +0200

    refactor: adjust HTML-formatting according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementsByClassName("new-task_js")[0];//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("todo");//ul of #itodo
-var completedTasksHolder=document.getElementById("completed");//completed
+var incompleteTaskHolder=document.getElementsByClassName("todo")[0];//ul of #itodo
+var completedTasksHolder=document.getElementsByClassName("completed")[0];//completed
 
 
 //New task list item
@@ -38,9 +38,9 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
-    checkBox.className="input checkbox"
+    checkBox.className="checkbox"
     editInput.type="text";
-    editInput.className="task input input_disabled intut_active";
+    editInput.className="task input_disabled input_active";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit button";
@@ -159,8 +159,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".edit");
+    var deleteButton=taskListItem.querySelector(".delete");
 
 
     //Bind editTask to edit button.
